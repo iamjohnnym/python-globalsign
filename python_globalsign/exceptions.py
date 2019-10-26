cd3ff20b107b6c3d85477faa8d7a5957ca16007b
John Martin <johnny@iamjohnnym.com>
2019-10-25 23:41:39 -0700

    handle exceptions for missing globalsign url attrs
    When missing the globalsign domain or wsdl path, we will raise an
    exception.
    
    GlobalSignDomainMissing
    
    : Raised when domain is missing
    
    GlobalSignWsdlPathMissing
    
    : Raised when WSDL path is missing

---
+++ python_globalsign/exceptions.py
@@ -0,0 +1,39 @@
+class Error(Exception):
+    """Base class for exceptions
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self,
+                 message="Python-GlobalSign has experienced an unexpected error"
+                 ):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        self.message = message
+
+
+class GlobalSignError(Error):
+    """Base class for handling exceptions for GlobalSign"""
+    def __init__(self,
+                 message="GlobalSign has experienced an error"
+                 ):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+        self.message = message
+
+
+class GlobalSignDomainMissing(Error):
+    """Handle exception for missing Globalsign Domain"""
+    def __init__(self):
+        # Call the base class constructor with the parameters it needs
+        self.message = "GlobalSign: Missing GlobalSign's SOAP domain"
+        super().__init__(self.message)
+
+
+class GlobalSignWsdlPathMissing(Error):
+    """Handle exception for missing Globalsign WSDL Url path"""
+    def __init__(self):
+        # Call the base class constructor with the parameters it needs
+        self.message = "GlobalSign: Missing GlobalSign's WSDL Url Path"
+        super().__init__(self.message)
